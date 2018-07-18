ce994c2f962a314ad4e030aa0ae1c1de7ae642be
Adilet <aitmatov40@gmail.com>
2018-07-18 13:30:24 +0600

    the_marker_is_made

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import {AppRegistry, View} from "react-native";
+import {AppRegistry, Image, View} from "react-native";
+import Icon from "expo/src/Icon";
 // import RetroMapStyles from './MapStyles/RetroMapStyles.json'
 
 class Map extends Component {
@@ -23,40 +24,58 @@ class Map extends Component {
         this.setState({ region });
     };
 
-    onMapPress = (e) => {
-        this.setState({
-            markers: [
-                ...this.state.markers,
-                {
-                    coordinate: e.nativeEvent.coordinate,
-                    key: `marker_${this.state.markers.length}`,
-                },
-            ],
-        });
-    };
+    // onMapPress = (e) => {
+    //     this.setState({
+    //         markers: [
+    //             ...this.state.markers,
+    //             {
+    //                 coordinate: e.nativeEvent.coordinate,
+    //                 key: `marker_${this.state.markers.length}`,
+    //             },
+    //         ],
+    //     });
+    // };
 
     render() {
         return (
             <View>
                 <MapView
                     provider={ PROVIDER_GOOGLE }
-                    style={{height: '100%', width: '100%'}}
+                    style={styles.map_container}
                     InitialRegion={this.state.region}
                     onRegionChange={this.onRegionChange}
-                    onPress={this.onMapPress}
+                    // onPress={this.onMapPress}
                 >
-                    {this.state.markers.map(marker => (
+
                         <MapView.Marker
-                            coordinate={marker.coordinate}
-                            key={marker.key}
+                            coordinate={this.state.region}
+                            // key={1}
                         />
-                    ))}
+                    {/*<Icon name="map-marker"*/}
+                          {/*style={{*/}
+                              {/*zIndex: 3,*/}
+                              {/*position: 'absolute',*/}
+                              {/*marginTop: -37,*/}
+                              {/*marginLeft: -11,*/}
+                              {/*left: '50%',*/}
+                              {/*top: '50%'}}*/}
+                          {/*size={40}*/}
+                          {/*color="#f00" />*/}
+                    {/*<Image source={require('./imagies/icon/map_marker.png')} style={styles.marker_icon}/>*/}
                 </MapView>
             </View>
         );
     }
 
 }
+
+const styles = {
+    map_container: {
+        height: '100%',
+        width: '100%'
+    },
+};
+
 export default Map;
 
 AppRegistry.registerComponent('Map', ()=> Map);
